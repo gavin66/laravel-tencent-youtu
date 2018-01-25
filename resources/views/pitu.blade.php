bf6755b464a52c2c0b388bf078d2e80cc7041788
Gavin <tianyu_0723@hotmail.com>
2018-01-25 16:34:20 +0800

    人像融合

---
+++ resources/views/pitu.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+    <b>hhhhh</b>
+    <img src="data:image/png;base64,{{ $imgage }}" alt="">
+    </body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//public static $APPID = '10116676';
+//public static $SECRET_ID = 'AKIDvjrvtkezKSRJzTM11pQoALNDrIagjPxJ';
+//public static $SECRET_KEY = 'rZSDalkW6XL3lKl02ObfYFj5FH7kDZIq';
+// QQ 896993802
+
+Route::post('/pitu',function (){
+    $image_base64 = \Illuminate\Support\Facades\Request::input('img_base64','');
+
+    \App\Service\YouTu\Conf::setAppInfo('10116676','AKIDvjrvtkezKSRJzTM11pQoALNDrIagjPxJ','rZSDalkW6XL3lKl02ObfYFj5FH7kDZIq','896993802');
+//    $image = file_get_contents('http://image.imspender.com/images/b009494be54b53167b61d3ce1d9339e2f51ee7c2.jpg');
+//    $image_base64 = base64_encode($image);
+    $uploadRet = \App\Service\YouTu\YouTu::pitu($image_base64);
+
+    return $uploadRet;
+});
